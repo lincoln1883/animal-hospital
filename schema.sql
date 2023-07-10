fb18db904c63bea7d7fce694038a825f647f8782
Lincoln Gibson <lincolngibson7@gmail.com>
2023-07-10 13:37:48 -0300

    Add table columns and constraints.

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name CHAR(25),
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg decimal
 );
