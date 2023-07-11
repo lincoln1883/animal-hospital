ca9753235348c3cedbe9dc47f9ea6c73ce8f8971
Lincoln Gibson <lincolngibson7@gmail.com>
2023-07-10 21:22:02 -0300

    Add delete and update the table with transactions

--- queries.sql
+++ queries.sql
@@ -21,3 +21,20 @@ UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 
 COMMIT;
+
+BEGIN
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+BEGIN
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT delete1;
+
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT delete1;
+
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
