8e8e3b181c9cdd2bfdd7fd6f81ad97a2553b837b
Lincoln Gibson <lincolngibson7@gmail.com>
2023-07-11 13:09:45 -0300

    Create species table with constraints.

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-    name CHAR(25),
-    date_of_birth date,
-    escape_attempts integer,
-    neutered boolean,
-    weight_kg decimal
+id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
+name CHAR(25),
+date_of_birth date,
+escape_attempts integer,
+neutered boolean,
+weight_kg decimal
 );
 
 ALTER TABLE animals ADD species CHAR(100);
@@ -15,4 +15,9 @@ CREATE TABLE owners(
 id integer NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 full_name CHAR(50),
 age integer,
+);
+
+CREATE TABLE species(
+id integer NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+name CHAR(50),
 );
