c756ee627ef552c39974e8cebcae410894185886
Lincoln Gibson <lincolngibson7@gmail.com>
2023-07-11 22:59:02 -0300

    Create specialization table for vets and species.

--- schema.sql
+++ schema.sql
@@ -36,3 +36,12 @@ name CHAR(50),
 age integer,
 date_of_graduation date
 );
+
+CREATE TABLE specializations (
+species_id integer, 
+vets_id integer,
+PRIMARY KEY (species_id, vet_id),
+FOREIGN KEY (species_id) REFERENCES species(id) ,
+FOREIGN KEY (vets_id) REFERENCES vets(id),
+);
+
