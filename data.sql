487baa07585f1b717c90fdfa0fd92e7df45ca883
Lincoln Gibson <lincolngibson7@gmail.com>
2023-07-18 14:37:23 -0300

    Insert data into the tables.

--- data.sql
+++ data.sql
@@ -75,10 +75,8 @@ LEFT JOIN animals AS a ON a.name = d.animal
 LEFT JOIN vets AS v ON v.name = d.vet;
 
 /*insert more data into visits*/
-INSERT INTO visits (animals_id, vets_id, date_of_visit)
-SELECT DISTINCT ON (animals_id,vets_id) animals_id.id, vets_id.id, visit_timestamp
-FROM (SELECT id FROM animals) AS animals_id,
-(SELECT id FROM vets) AS vets_id,
-generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') AS visit_timestamp;
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animals_id, (SELECT id FROM vets) vets_id, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
 
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
 INSERT INTO owners (full_name, email) SELECT 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
